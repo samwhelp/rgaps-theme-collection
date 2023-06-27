0c0909b43aab4f350a8b4a9ed428a17eb8745093
samwhelp <samwhelp@users.noreply.github.com>
2023-06-27 16:44:13 +0800

    fix

---
+++ _demo/prototype/rgaps-theme-installer/config-rgaps-blend.sh
@@ -0,0 +1,83 @@
+#!/usr/bin/env bash
+
+
+################################################################################
+### Head: Note
+##
+
+## * https://github.com/samwhelp/skel-project-plan/blob/gh-pages/demo/sh/project/bin/config.sh
+
+##
+### Tail: Note
+################################################################################
+
+
+################################################################################
+### Head: Init
+##
+
+THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+THE_PLAN_DIR_PATH="${THE_BASE_DIR_PATH}"
+THE_CONFIG_FILE_PATH="${THE_PLAN_DIR_PATH}/config.sh"
+THE_UTIL_FILE_PATH="${THE_PLAN_DIR_PATH}/util.sh"
+
+source "${THE_CONFIG_FILE_PATH}"
+source "${THE_UTIL_FILE_PATH}"
+
+##
+### Tail: Init
+################################################################################
+
+
+
+
+################################################################################
+### Head: Model / Config / rgaps-blend
+##
+
+mod_config_xfwm_theme_rgaps_blend () {
+
+	util_error_echo 'xfconf-query -c "xfwm4" -p "/general/theme" -s "RGapsBlend"'
+	xfconf-query -c "xfwm4" -p "/general/theme" -s "RGapsBlend"
+
+}
+
+##
+### Tail: Model / Config / rgaps-blend
+################################################################################
+
+
+################################################################################
+### Head: Model / Config
+##
+
+mod_config () {
+
+	mod_config_xfwm_theme_rgaps_blend
+
+	return 0
+
+}
+
+##
+### Tail: Model / Config
+################################################################################
+
+
+
+
+################################################################################
+### Head: Main
+##
+
+__main__ () {
+
+	mod_config
+
+}
+
+__main__
+
+##
+### Tail: Main
+################################################################################
